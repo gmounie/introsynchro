8a4dab17096464616a42a08ecaa8c1201204cbef
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-08-04 21:20:20 +0200

    version en go

---
+++ Codeparallelisme/startGo.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func hello(i int, c chan int) {
+	fmt.Println("Hello world! de ", i)
+	c <- 1
+}
+
+func main() {
+	fini := make(chan int)
+	go hello(0, fini)
+	go hello(2, fini)
+	<- fini
+	<- fini
+}
