01d435a4b64a3c84a79698012707efa70aa44f60
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-08-16 15:15:42 +0200

    fibo en c++ pour le passage de future

---
+++ CodeFibo/fiboCpp.cpp
@@ -0,0 +1,24 @@
+#include <thread>
+#include <cassert>
+#include <iostream>
+#include <future>
+
+long long int fibo(int n) {
+  if (n < 2)
+    return 1;
+
+  auto res1 = std::async(fibo, n-1);
+  long long int res2 = fibo(n-2);
+  return res1.get() + res2;
+}
+
+int main(int argc, char **argv) {
+  assert(argc == 2);
+  int n = atoi(argv[1]);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  long long int resu = fibo(n);
+  auto end = std::chrono::high_resolution_clock::now();
+  double temps = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  std::cout << "fibo(" << n << ")" << " = " << resu << " en " << temps/1000. << "s" << std::endl;
+}
