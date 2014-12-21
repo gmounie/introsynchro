1e96b5cdc82ac00c2191684123bf115ae7ba9de4
Grégory Mounié <Gregory.Mounie@imag.fr>
2014-12-21 15:07:46 +0100

    intptr_t + sleep 5

--- Codeparallelisme/startC99.c
+++ Codeparallelisme/startC99.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define NBTHR 10
 
 void *hello(void *a) {
-  printf ("hello world! de %ld\n", (long)a);
+  printf ("hello world! de %ld\n", (intptr_t)a);
+  sleep(5);
   return 0;
 }
 
 int main(int argc, char **argv) {
   (void)argc;
   (void) argv;
-  pthread_t tid[2];
+  pthread_t tid[NBTHR];
   
-  for (int i=0; i < 2; i++) {
-    pthread_create(& tid[i], NULL, hello, (void *)(long)i);
+  for (int i=0; i < NBTHR; i++) {
+    pthread_create(& tid[i], NULL, hello, (void *)(intptr_t)i);
   }
   
-  for (int i=0; i < 2; i++) {
+  for (int i=0; i < NBTHR; i++) {
     void *status;
     pthread_join(tid[i], & status);
   }
