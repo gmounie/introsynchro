ce98c97e5dd62a906dd12b6872fd2c41ecb39941
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-01-11 10:51:34 +0100

    using std

--- Codeparallelisme/startCpp.cpp
+++ Codeparallelisme/startCpp.cpp
@@ -4,26 +4,27 @@
 #include <vector>
 
 constexpr int NBTHR= 10;
+using namespace std;
 
 void hello(int a) {
-  std::cout << "hello world! de " << a << std::endl;
-  std::chrono::seconds d{5};
-  std::this_thread::sleep_for(d);
+  cout << "hello world! de " << a << endl;
+  chrono::seconds d{5};
+  this_thread::sleep_for(d);
 }
 
 int main(int argc, char **argv) {
   (void)argc;
   (void) argv;
-  std::vector<std::thread *> vtid(NBTHR);
+  vector<thread *> vtid(NBTHR);
 
   int i=0;
   for (auto &t: vtid) {
-    t = new std::thread(hello, i++);
+    t = new thread(hello, i++);
   }
-  
+
   for (auto const t: vtid) {
     t->join();
   }
-  
+
   return 0;
 }
