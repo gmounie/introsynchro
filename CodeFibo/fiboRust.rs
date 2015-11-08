7955f3dd6488fbac063d429210ffb7caec5caefd
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-08 21:55:36 +0100

    fibo // en rust

---
+++ CodeFibo/fiboRust.rs
@@ -0,0 +1,26 @@
+use std::env::args;
+use std::thread;
+
+fn fibo(n : &u64) -> u64 {
+    let v1:u64;
+    let mut v2:u64 = 0;
+    if *n < 2 {
+        v1 = 1
+    }
+    else {
+        let arg = n - 1;
+        let handle = thread::spawn(move || { fibo(&arg)});
+        let arg2 = n-2;
+        v1 = fibo(&arg2);
+        v2 = handle.join().unwrap()
+    }
+    v1 + v2
+}
+
+
+fn main() {
+    let a =  args();
+    let n:u64 = a.last().expect("no args at all")
+             .trim().parse().ok().expect("an integer as last argument");
+    println!("{}", fibo(&n));
+}
