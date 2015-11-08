49bfe3a77690f0007a951f791d33a169501cfe00
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-08 22:07:18 +0100

    enlever les reference inutile de la version rust

--- CodeFibo/fiboRust.rs
+++ CodeFibo/fiboRust.rs
@@ -1,18 +1,18 @@
 use std::env::args;
 use std::thread;
 
-fn fibo(n : &u64) -> u64 {
+fn fibo(n : u64) -> u64 {
     let v1:u64;
     let mut v2:u64 = 0;
-    if *n < 2 {
+    if n < 2 {
         v1 = 1
     }
     else {
-        let arg = n - 1;
-        let handle = thread::spawn(move || { fibo(&arg)});
-        let arg2 = n-2;
-        v1 = fibo(&arg2);
-        v2 = handle.join().unwrap()
+        let arg1 = n - 1;
+        let handle = thread::spawn(move || { fibo(arg1)});
+        let arg2 = n - 2;
+        v2 = fibo(arg2);
+        v1 = handle.join().unwrap()
     }
     v1 + v2
 }
@@ -22,5 +22,5 @@ fn main() {
     let a =  args();
     let n:u64 = a.last().expect("no args at all")
              .trim().parse().ok().expect("an integer as last argument");
-    println!("{}", fibo(&n));
+    println!("{}", fibo(n));
 }
